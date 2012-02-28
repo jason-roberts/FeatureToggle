292ca18030008d85a436b66464ce1279927a00ca
Jason Roberts <osjasonroberts@gmail.com>
2012-02-28 19:58:53 +0800

    cleanup the date based toggles

--- src/FeatureToggleSolution/Tests/FeatureToggle.Integration.Tests/EnabledOnOrAfterDateFeatureToggleTests.cs
+++ src/FeatureToggleSolution/Tests/FeatureToggle.Integration.Tests/EnabledOnOrAfterDateFeatureToggleTests.cs
@@ -9,29 +9,43 @@ namespace FeatureToggle.Integration.Tests
     public class EnabledOnOrAfterDateFeatureToggleTests
     {
         [TestMethod]
-        public void ShouldBeEnabledAfterDate()
+        public void ShouldBeEnabledOnDate()
         {
             var mockDate = new Mock<INowDateAndTime>();
-            
             mockDate.SetupGet(x => x.Now).Returns(new DateTime(2000, 1, 1, 0, 0, 0, 0));
-
-            var sut = new NewYearsDay2000 { NowProvider = mockDate.Object };
+            var sut = new NewYearsDay2000 {NowProvider = mockDate.Object};
 
             Assert.IsTrue(sut.FeatureEnabled);
         }
+
+
+        [TestMethod]
+        public void ShouldBeEnabledAfterDate()
+        {
+            var mockDate = new Mock<INowDateAndTime>();
+
+            mockDate.SetupGet(x => x.Now).Returns(new DateTime(2000, 1, 1, 0, 0, 0, 1));
+
+            var sut = new NewYearsDay2000 {NowProvider = mockDate.Object};
+
+            Assert.IsTrue(sut.FeatureEnabled);
+        }
+
 
         [TestMethod]
         public void ShouldBeDisabledBeforeDate()
         {
             var mockDate = new Mock<INowDateAndTime>();
-            mockDate.SetupGet(x => x.Now).Returns(new DateTime(2000, 1, 1, 0, 0, 0, 0).Subtract(TimeSpan.FromMilliseconds(1)));
+            mockDate.SetupGet(x => x.Now).Returns(
+                new DateTime(2000, 1, 1, 0, 0, 0, 0).Subtract(TimeSpan.FromMilliseconds(1)));
 
-            var sut = new NewYearsDay2000 { NowProvider = mockDate.Object };
+            var sut = new NewYearsDay2000 {NowProvider = mockDate.Object};
 
             Assert.IsFalse(sut.FeatureEnabled);
         }
 
-        private class NewYearsDay2000 : EnabledOnOrAfterDateFeatureToggle { }
-    }
 
+        private class NewYearsDay2000 : EnabledOnOrAfterDateFeatureToggle{}
+
+    }
 }
