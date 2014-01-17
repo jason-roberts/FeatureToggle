f5182345c26e2925b9f94330022485322975f94a
Avner Shanan <ashanan@gmail.com>
2014-01-17 12:19:31 -0600

    Fix accidental change to copyright symbol

--- src/FeatureToggleSolution/SharedAssemblyInfo.cs
+++ src/FeatureToggleSolution/SharedAssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("FeatureToggle")]
-[assembly: AssemblyCopyright("Copyright � Jason Roberts 2011")]
+[assembly: AssemblyCopyright("Copyright © Jason Roberts 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
