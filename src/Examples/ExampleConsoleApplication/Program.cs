9e0a1be7cb231227eb58095d8ed852460570bc4e
Jason Roberts <osjasonroberts@gmail.com>
2014-04-21 10:45:51 +0800

    Added initial Console example #23

---
+++ src/Examples/ExampleConsoleApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FeatureToggle;
+
+namespace ExampleConsoleApplication
+{
+    // Define a new toggle
+    // SimpleFeatureToggle get it's value from appSettings by default.
+    //
+    // <appSettings>
+    //  <add key="FeatureToggle.ShowMessageToggle" value="true"/>
+    //</appSettings>
+    // Note the "FeatureToggle." prefix on the key - this is required
+    class ShowMessageToggle : SimpleFeatureToggle
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toggle = new ShowMessageToggle();
+
+            if (toggle.FeatureEnabled)
+            {
+                Console.WriteLine("This feature is enabled");
+            }
+            else
+            {
+                // to disable the feature:
+                //  <add key="FeatureToggle.ShowMessageToggle" value="false"/>
+                Console.WriteLine("This feature is disabled");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
