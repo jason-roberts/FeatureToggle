c2f5049edb5af6db4f68a0244c4cdea8a9f5f12b
Jason Roberts <osjasonroberts@gmail.com>
2014-04-25 18:23:23 +0800

    Added value convertor for windows store apps #24

--- src/FeatureToggle.WindowsStore/Toggles/FeatureToggleToVisibilityConverter.cs
+++ src/FeatureToggle.WindowsStore/Toggles/FeatureToggleToVisibilityConverter.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 using FeatureToggle.Core;
 
 namespace FeatureToggle.Toggles
-{
+{    
     public class FeatureToggleToVisibilityConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, string language)
         {
             var toggle = (IFeatureToggle)value;
 
             return toggle.FeatureEnabled ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            throw new NotSupportedException();
+            throw new NotImplementedException();
         }
     }
 }
