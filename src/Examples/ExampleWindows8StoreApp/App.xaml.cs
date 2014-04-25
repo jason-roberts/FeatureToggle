9aa05567b4c3afcb0d217662c155114f1f236e44
Jason Roberts <osjasonroberts@gmail.com>
2014-04-25 20:30:35 +0800

    Removing unused refs #26

--- src/Examples/ExampleWindows8StoreApp/App.xaml.cs
+++ src/Examples/ExampleWindows8StoreApp/App.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
