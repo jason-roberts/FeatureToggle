5aaa540b3a0cdd48d9f858b148f13ce7c86ced39
Jason Roberts <osjasonroberts@gmail.com>
2014-04-28 17:09:08 +0800

    WindowsPhone8 NuGet
    Also updated .ignore to allow build folder in nuget package structure to
    be included

--- build-all.bat
+++ build-all.bat
@@ -9,15 +9,25 @@ msbuild src\WindowsStoreBuild.sln /t:Rebuild /p:Configuration=Release /p:Platfor
 msbuild src\WindowsStoreBuild.sln /t:Rebuild /p:Configuration=Release /p:Platform=ARM /verbosity:q
 
 
+echo building platform specific windows phone
+msbuild src\WindowsPhoneBuild.sln /t:Rebuild /p:Configuration=Release /p:Platform=ARM /verbosity:q
+msbuild src\WindowsPhoneBuild.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /verbosity:q
+
+
 echo copying nuget package files
 
 copy src\FeatureToggle.Core\bin\Release\FeatureToggle.Core.dll "NuGet\FeatureToggle.Core\lib\portable-net45+sl50+wp80+win"
+
 copy src\FeatureToggle\bin\Release\FeatureToggle.dll NuGet\FeatureToggle\lib\net40
+
 copy src\FeatureToggle.WindowsStore\bin\x86\Release\FeatureToggle.WindowsStore.dll NuGet\FeatureToggle\lib\netcore451
 copy src\FeatureToggle.WindowsStore\bin\x86\Release\FeatureToggle.WindowsStore.dll NuGet\FeatureToggle\build\netcore451\x86
 copy src\FeatureToggle.WindowsStore\bin\x64\Release\FeatureToggle.WindowsStore.dll NuGet\FeatureToggle\build\netcore451\x64
 copy src\FeatureToggle.WindowsStore\bin\ARM\Release\FeatureToggle.WindowsStore.dll NuGet\FeatureToggle\build\netcore451\ARM
 
-
+copy src\FeatureToggle.WindowsPhone\bin\x86\Release\FeatureToggle.WindowsPhone.dll NuGet\FeatureToggle\lib\windowsphone8
+copy src\FeatureToggle.WindowsPhone\bin\x86\Release\FeatureToggle.WindowsPhone.dll NuGet\FeatureToggle\build\windowsphone8\x86
+copy src\FeatureToggle.WindowsPhone\bin\ARM\Release\FeatureToggle.WindowsPhone.dll NuGet\FeatureToggle\build\windowsphone8\ARM
+     
 build-nuget.bat
 
---
+++ src/Tests/ManualNuGetTestingProjects/NugetPackageTests/PhoneApp1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneApp1.Resources;
+
+namespace PhoneApp1
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
