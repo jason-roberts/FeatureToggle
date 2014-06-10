0389187e8aebd896a3278618dfdd7201aa73e4c0
Jason Roberts <osjasonroberts@gmail.com>
2014-06-10 14:02:18 +0800

    RandomFeatureToggle complete #43

--- src/FeatureToggle.Core/RandomFeatureToggle.cs
+++ src/FeatureToggle.Core/RandomFeatureToggle.cs
@@ -3,15 +3,40 @@
 namespace FeatureToggle.Core
 {
     public class RandomFeatureToggle : IFeatureToggle
-    {
+    {        
         public bool FeatureEnabled
         {
             get
-            {
-                
+            {               
+                return RandomGenerator.Next() % 2 == 0;                
+            }
+        }
 
-                
-                return true;
+
+        // Based on: http://blogs.msdn.com/b/pfxteam/archive/2009/02/19/9434171.aspx
+        static class RandomGenerator
+        {
+            private static readonly Random NonThreadLocalInstance = new Random();
+
+            [ThreadStatic]
+            private static Random _threadLocalInstance;
+
+            public static int Next()
+            {
+                var rnd = _threadLocalInstance;
+
+                if (rnd != null)
+                {
+                    return rnd.Next();
+                }
+
+                int seed;
+
+                lock (NonThreadLocalInstance) seed = NonThreadLocalInstance.Next();
+
+                _threadLocalInstance = rnd = new Random(seed);
+
+                return rnd.Next();
             }
         }
     }
