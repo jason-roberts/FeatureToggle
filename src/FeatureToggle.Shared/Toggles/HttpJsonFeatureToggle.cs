c250b17124bc17b8c2e12df5a466802fdf649bb9
Jason Roberts <osjasonroberts@gmail.com>
2014-06-20 09:33:47 +0800

    Started on #50

---
+++ src/FeatureToggle.Shared/Toggles/HttpJsonFeatureToggle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+#if (FEATURETOGGLE_FULL)
+using FeatureToggle.Core;
+using FeatureToggle.Providers;
+
+namespace FeatureToggle.Toggles
+{
+    public abstract class HttpJsonFeatureToggle : IFeatureToggle
+    {
+        protected HttpJsonFeatureToggle()
+        {
+            //ToggleValueProvider = new BooleanSqlServerProvider();
+        }
+
+
+        public virtual IBooleanToggleValueProvider ToggleValueProvider { get; set; }
+
+
+        public bool FeatureEnabled
+        {
+            get
+            {
+                throw new NotImplementedException();
+                //return ToggleValueProvider.EvaluateBooleanToggleValue(this);
+            }
+        }
+
+    }
+}
+
+#endif
---
+++ src/Tests/FeatureToggle.Integration.Tests/BooleanHttpJsonProviderShould.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using FeatureToggle.Core;
+using FeatureToggle.Providers;
+using FeatureToggle.Toggles;
+using Xunit;
+
+namespace FeatureToggle.Integration.Tests
+{    
+    public class BooleanHttpJsonProviderShould
+    {
+        //[Fact]
+        //public void ReadBooleanTrueFromSqlServer()
+        //{            
+        //    var sut = new BooleanSqlServerProvider();
+
+        //    var toggle = new MySqlServerToggleTrue();
+
+        //    Assert.True(sut.EvaluateBooleanToggleValue(toggle));
+        //}
+
+
+        //[Fact]
+        //public void ReadBooleanFalseFromSqlServer()
+        //{
+        //    var sut = new BooleanSqlServerProvider();
+
+        //    var toggle = new MySqlServerToggleFalse();
+
+        //    Assert.False(sut.EvaluateBooleanToggleValue(toggle));
+        //}
+
+
+
+        [Fact]
+        public void ErrorWhenUrlNotInConfig()
+        {
+            var sut = new MissingUrlToggle();
+
+            Assert.Throws<ToggleConfigurationError>(() => sut.FeatureEnabled);            
+        }
+
+
+
+        private class MissingUrlToggle : HttpJsonFeatureToggle { }
+
+    }
+}
