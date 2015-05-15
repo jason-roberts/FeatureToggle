6a1e0b2d2dd0657566ddf92c2d9ba07827750e25
Jason Roberts <osjasonroberts@gmail.com>
2015-05-15 11:16:21 +0800

    Initial work for #64

---
+++ src/FeatureToggle.Core/SimpleCachedDecorator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FeatureToggle.Core
+{
+    public class SimpleCachedDecorator : IFeatureToggle
+    {
+        private readonly TimeSpan _cacheDuration;
+        private bool _cachedValue;
+        private DateTime _cachedValueLastUpdatedTime;
+        private DateTime _cacheExpiryTime;
+        public IFeatureToggle WrappedToggle { get; private set; }
+
+        public SimpleCachedDecorator(IFeatureToggle toggleToWrap, TimeSpan cacheDuration)
+        {            
+            if (toggleToWrap == null)
+            {
+                throw new ArgumentNullException("toggleToWrap");
+            }
+
+            WrappedToggle = toggleToWrap;
+            _cacheDuration = cacheDuration;
+
+            SetCachedValue();
+        }
+
+        private void SetCachedValue()
+        {
+            _cachedValue = WrappedToggle.FeatureEnabled;
+
+            _cachedValueLastUpdatedTime = DateTime.Now;
+            _cacheExpiryTime = _cachedValueLastUpdatedTime.Add(_cacheDuration);
+        }
+
+
+        public bool FeatureEnabled
+        {
+            get
+            {
+                var cacheHasExpired = DateTime.Now > _cacheExpiryTime;
+
+                if (cacheHasExpired)
+                {
+                    SetCachedValue();
+                }
+
+                return _cachedValue;
+            }
+        }
+    }
+}
---
+++ src/Tests/FeatureToggle.Tests/SimpleCacheDecoratorShould.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using FeatureToggle.Core;
+using Xunit;
+
+namespace FeatureToggle.Tests
+{
+    public class SimpleCacheDecoratorShould
+    {      
+        [Fact]
+        public void ErrorWhenNullWrappedTogglesSupplied()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SimpleCachedDecorator(null, TimeSpan.Zero));
+        }
+
+
+        [Fact]
+        public void RetrieveInitialValueFromWrappedToggle()
+        {
+            var cachedToggle = new CacheTestToggle();
+            cachedToggle.Enable();
+
+            var sut = new SimpleCachedDecorator(cachedToggle, TimeSpan.FromSeconds(1));
+
+            Assert.True(sut.FeatureEnabled);
+        }
+
+
+        [Fact]
+        public void MaintainInitialValueWithinCacheDurationWhenUnderlyingToggleValueChanges()
+        {
+            var cachedToggle = new CacheTestToggle();
+            cachedToggle.Enable();
+
+            var sut = new SimpleCachedDecorator(cachedToggle, TimeSpan.FromSeconds(1));
+
+            cachedToggle.Disable();
+
+            Assert.True(sut.FeatureEnabled);
+        }
+
+
+        [Fact]
+        public void UpdateNewValueAfterCacheDurationWhenUnderlyingToggleValueChanged()
+        {
+            var cachedToggle = new CacheTestToggle();
+            cachedToggle.Enable();
+
+            var sut = new SimpleCachedDecorator(cachedToggle, TimeSpan.FromSeconds(1));
+
+            cachedToggle.Disable();
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            Assert.False(sut.FeatureEnabled);
+        }
+
+        
+        private class CacheTestToggle : IFeatureToggle
+        {
+            private bool _isEnabled;
+
+            public void Enable()
+            {
+                _isEnabled = true;
+            }
+
+            public void Disable()
+            {
+                _isEnabled = false;
+            }
+
+            public bool FeatureEnabled
+            {
+                get
+                {
+                    return _isEnabled;                    
+                }
+            }
+        }
+    }
+
+    
+}
