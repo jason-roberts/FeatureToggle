8160c83358712a9c6ae1b797f4a71bc13ab05bcd
Bastian John <mail@bastianjohn.com>
2015-06-03 13:33:31 +0200

    Handled a ConnectionStringName in the BooleanSQLServerProvider class to prevent duplicated connection strings

--- src/FeatureToggle.Shared/Providers/BooleanSqlServerProvider.cs
+++ src/FeatureToggle.Shared/Providers/BooleanSqlServerProvider.cs
@@ -29,12 +29,32 @@ namespace FeatureToggle.Providers
         private string GetConnectionStringFromConfig(IFeatureToggle toggle)
         {
             var toggleNameInConfig = ToggleConfigurationSettings.Prefix + toggle.GetType().Name + ".ConnectionString";
+            var toggleConnectionNameInConfig = ToggleConfigurationSettings.Prefix + toggle.GetType().Name + ".ConnectionStringName";
 
-            if (!ConfigurationManager.AppSettings.AllKeys.Contains(toggleNameInConfig))
-                throw new ToggleConfigurationError(string.Format("The key '{0}' was not found in AppSettings",
-                                                                     toggleNameInConfig));
+            //Check if either a ConnectionString or a ConnectionStringName exists
+            if (!ConfigurationManager.AppSettings.AllKeys.Contains(toggleNameInConfig) && !ConfigurationManager.AppSettings.AllKeys.Contains(toggleConnectionNameInConfig))
+                throw new ToggleConfigurationError(string.Format("The key '{0}' or '{1}' was not found in AppSettings",
+                                                                     toggleNameInConfig, 
+                                                                     toggleConnectionNameInConfig));
 
-            return ConfigurationManager.AppSettings[toggleNameInConfig];
+            string connectionString;
+            //Use the ConnectionStringName if it's given
+            if(ConfigurationManager.AppSettings.AllKeys.Contains(toggleConnectionNameInConfig))
+            {
+                string connectionStringName = ConfigurationManager.AppSettings[toggleConnectionNameInConfig];
+                ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringName];
+                if (connectionStringSettings == null || string.IsNullOrEmpty(connectionStringSettings.ConnectionString))
+                    throw new ToggleConfigurationError(string.Format("The connectionString with Key '{0}' was not found or is empty in the web.config file.",
+                                                                     connectionStringName));
+                connectionString = connectionStringSettings.ConnectionString;
+            }
+            else
+            {
+                connectionString = ConfigurationManager.AppSettings[toggleNameInConfig];
+            }
+
+
+            return connectionString;
         }
 
         private string GetCommandTextFromAppConfig(IFeatureToggle toggle)
