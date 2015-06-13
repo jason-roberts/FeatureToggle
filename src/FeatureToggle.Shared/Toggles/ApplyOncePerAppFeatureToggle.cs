c129d896921344b9a66a05b8596bac17a1b4879b
Odonno <bottiau.david@laposte.net>
2015-06-13 11:36:18 +0200

    Create FeatureToggles based on ApplyOnce

---
+++ src/FeatureToggle.Shared/Toggles/ApplyOncePerAppFeatureToggle.cs
@@ -0,0 +1,43 @@
+﻿#if NETFX_CORE || WINDOWS_PHONE
+using Windows.Storage;
+using FeatureToggle.Core;
+
+// ReSharper disable CheckNamespace
+namespace FeatureToggle.Toggles
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// This toggle is activated only one time per app, 
+    /// Retrieve toggle through Local Settings
+    /// </summary>
+    public class ApplyOncePerAppFeatureToggle : IFeatureToggle
+    {
+        // BUG : Local Settings are remove when app is removed (save in Roaming with device ID ?)
+        public bool FeatureEnabled
+        {
+            get
+            {
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey(Name))
+                    return (bool)ApplicationData.Current.LocalSettings.Values[Name];
+                return true;
+            }
+            private set
+            {
+                ApplicationData.Current.LocalSettings.Values[Name] = value;
+            }
+        }
+
+        public string Name { get; private set; }
+
+
+        /// <summary>
+        /// Apply the use of the toggle (if it was enabled, disable it)
+        /// </summary>
+        public virtual void Apply()
+        {
+            if (FeatureEnabled)
+                FeatureEnabled = false;
+        }
+    }
+}
+#endif
---
+++ src/FeatureToggle.Shared/Toggles/ApplyOncePerUserFeatureToggle.cs
@@ -0,0 +1,43 @@
+﻿#if NETFX_CORE || WINDOWS_PHONE
+using Windows.Storage;
+using FeatureToggle.Core;
+
+// ReSharper disable CheckNamespace
+namespace FeatureToggle.Toggles
+// ReSharper restore CheckNamespace
+{
+    /// <summary>
+    /// This toggle is activated only one time per user, per all device family of the user
+    /// Example : Explains how the app works only one time (no matter how many devices user have)
+    /// Retrieve toggle through Roaming Settings
+    /// </summary>
+    public class ApplyOncePerUserFeatureToggle : IFeatureToggle
+    {
+        public bool FeatureEnabled
+        {
+            get
+            {
+                if (ApplicationData.Current.RoamingSettings.Values.ContainsKey(Name))
+                    return (bool)ApplicationData.Current.RoamingSettings.Values[Name];
+                return true;
+            }
+            private set
+            {
+                ApplicationData.Current.RoamingSettings.Values[Name] = value;
+            }
+        }
+
+        public string Name { get; private set; }
+
+
+        /// <summary>
+        /// Apply the use of the toggle (if it was enabled, disable it)
+        /// </summary>
+        public virtual void Apply()
+        {
+            if (FeatureEnabled)
+                FeatureEnabled = false;
+        }
+    }
+}
+#endif
