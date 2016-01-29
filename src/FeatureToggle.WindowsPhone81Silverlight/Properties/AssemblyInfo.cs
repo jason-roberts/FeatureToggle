55981536ead95fb8610d85f4547ea56246549a2d
Jason Roberts <osjasonroberts@gmail.com>
2016-01-29 14:23:12 +0800

    Updated assemblyinfos for v3.3

--- src/FeatureToggle.WindowsPhone81Silverlight/Properties/AssemblyInfo.cs
+++ src/FeatureToggle.WindowsPhone81Silverlight/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e80213df-1e94-4748-90ed-9a527bfd2371")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
