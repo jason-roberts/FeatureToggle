c15f7541e8c54b5492dac9fd69a5a50e82b90ed1
Jason Roberts <osjasonroberts@gmail.com>
2016-02-17 17:10:59 +0800

    Part progress on #111

---
+++ src/FeatureToggle.Core/FallbackValueDecorator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FeatureToggle.Core
+{
+
+    public class FallbackValueDecorator : IFeatureToggle
+    {
+        public IFeatureToggle PrimaryToggle { get; private set; }
+        public IFeatureToggle FallbackToggle { get; private set; }
+        private readonly Action<Exception> _logAction;
+
+        public FallbackValueDecorator(IFeatureToggle primaryToggle, IFeatureToggle fallbackToggle, Action<Exception> logAction = null)
+        {
+            if (primaryToggle == null)
+            {
+                throw new ArgumentNullException(nameof(primaryToggle));
+            }
+
+            if (fallbackToggle == null)
+            {
+                throw new ArgumentNullException(nameof(fallbackToggle));
+            }
+
+            PrimaryToggle = primaryToggle;
+            FallbackToggle = fallbackToggle;
+            _logAction = logAction;
+        }
+
+        public bool FeatureEnabled
+        {
+            get
+            {
+                try
+                {
+                    return PrimaryToggle.FeatureEnabled;
+                }
+                catch (Exception ex)
+                {                    
+                    return FallbackToggle.FeatureEnabled;                    
+                }
+            }
+        }
+    }
+}
---
+++ src/Tests/FeatureToggle.Tests/FallbackValueDecoratorShould.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FeatureToggle.Core;
+using Xunit;
+
+namespace FeatureToggle.Tests
+{
+    public class FallbackValueDecoratorShould
+    {
+        [Fact]
+        public void ReturnValueOfPrimaryToggleIfAvailable()
+        {
+            var sut = new FallbackValueDecorator(new AnEnabledFeature(), new ADisabledFeature());
+
+            Assert.True(sut.FeatureEnabled);
+        }
+
+
+        [Fact]
+        public void ReturnValueOfFallbackToggleIfPrimaryToggleNotCOnfiguredOrErrors()
+        {
+            var sut = new FallbackValueDecorator(new AnErroringToggle(), new AnEnabledFeature());
+
+            Assert.True(sut.FeatureEnabled);
+        }
+
+
+        [Fact]
+        public void ErrorIfFallbackToggleErrors()
+        {
+            var sut = new FallbackValueDecorator(new AnErroringToggle(), new AnErroringToggle());
+
+            Assert.Throws<ToggleConfigurationError>(() => sut.FeatureEnabled);
+        }
+
+
+
+        [Fact]
+        public void ErrorWhenNullPrimaryToggleSupplied()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FallbackValueDecorator(null, new AnErroringToggle()));
+        }
+
+
+        [Fact]
+        public void ErrorWhenNullFallbackToggleSupplied()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FallbackValueDecorator(new AnEnabledFeature(), null));
+        }
+
+
+
+
+
+        private class AnEnabledFeature : AlwaysOnFeatureToggle {}
+        private class ADisabledFeature : AlwaysOffFeatureToggle {}
+
+        private class AnErroringToggle : IFeatureToggle
+        {
+            public bool FeatureEnabled
+            {
+                get
+                {
+                    throw new ToggleConfigurationError("Simulated toggle exception");                     
+                }
+            }
+        }
+
+  
+    }
+}
