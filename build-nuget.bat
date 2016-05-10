5be33fb6f1ca8bb88b134a53ab9897d328d14532
Jason Roberts <osjasonroberts@gmail.com>
2016-05-10 11:47:45 +0800

    Updated build/package to delete dlls

--- build-nuget.bat
+++ build-nuget.bat
@@ -5,12 +5,15 @@ echo Building core PCL package
 NuGet\nuget pack NuGet\FeatureToggle.Core\FeatureToggle.Core.nuspec
 
 
-
 echo Building main package
 NuGet\nuget pack NuGet\FeatureToggle\FeatureToggle.nuspec
 
 
 echo Building RavenDB
 NuGet\nuget pack NuGet\FeatureToggle.RavenDB\FeatureToggle.RavenDB.nuspec
+
+
+echo delete packaging dlls
+del /s "NuGet\*.dll"
 
 pause
