1bd484b0236abef95f09666f0a7fe9c777db4471
Jason Roberts <osjasonroberts@gmail.com>
2016-09-06 14:26:23 +0800

    Closes #117

--- src/Tests/ManualNuGetTestingProjects/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ src/Tests/ManualNuGetTestingProjects/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConsoleApplication1.SampleFeatureToggles;
+using FeatureToggle.Core;
 using FeatureToggle.Core.Fluent;
 
 namespace ConsoleApplication1
 {
+
+    class SampleFeatureToggle : AlwaysOnFeatureToggle { }
+
     class Program
     {
         static void Main(string[] args)
--- src/Tests/ManualNuGetTestingProjects/ConsoleApplication1/ConsoleApplication1/SampleFeatureToggles/SampleFeatureToggle.cs
+++
@@ -1,6 +0,0 @@
-using FeatureToggle.Toggles;
-
-namespace ConsoleApplication1.SampleFeatureToggles
-{
-    public class SampleFeatureToggle : SimpleFeatureToggle { }
-}
