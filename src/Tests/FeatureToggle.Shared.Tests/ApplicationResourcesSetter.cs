48415f7fefe93f028dc2473113b1a05b64d21127
Jason Roberts <osjasonroberts@gmail.com>
2017-03-29 15:19:03 +0800

    initial shared tests

---
+++ src/Tests/FeatureToggle.Shared.Tests/ApplicationResourcesSetter.cs
@@ -0,0 +1,20 @@
+﻿#if NETFX_CORE    
+
+using Windows.UI.Xaml;
+// ReSharper disable CheckNamespace
+namespace FeatureToggle.Shared.Tests
+// ReSharper restore CheckNamespace
+{
+    public static class ApplicationResourcesSetter
+    {
+        public static void Set(string key, object value)
+        {
+         
+
+
+      Application.Current.Resources[key] = value;
+
+        }
+    }
+}
+#endif
